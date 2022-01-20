dbb9073a37439ba98d3b80edeb02dcf6af345b82
Franz Reiter <franz.reiter@gcad3d.org>
2022-01-20 08:58:13 +0100

    V-2.46.25-1 - bugfix build-script ../init_examples.sh

--- src/init_examples.sh
+++ src/init_examples.sh
@@ -2,23 +2,28 @@
 #
 # unpack examples.gz into test-environment (NOT INTO install-directory)
 # use it for testing gcad3d without installation.
-# unpack ${DIR_DEV}gcad3d/packages/examples.gz into ${DIR_DEV}gcad3d/gCAD3D/.
+# unpack ${gcad_dir_dev}packages/examples.gz into ${gcad_dir_dev}gCAD3D/.
+# for ${gcad_dir_dev} use options.sh
 
 
-fnam=${gcad_dir_bas}packages/examples.gz
+fnam=${gcad_dir_dev}packages/examples.gz
 if [ ! -f ${fnam} ]; then
   echo "****** ERROR - file $fnam does not exist. *******"
   exit 2
+else
+  echo "- unpack "${fnam}
 fi
 
-mkdir -p ${DIR_DEV}gcad3d
-mkdir -p ${DIR_DEV}gcad3d/gCAD3D
+mkdir -p ${gcad_dir_dev}
+mkdir -p ${gcad_dir_dev}gCAD3D
 
-cd ${DIR_DEV}gcad3d/gCAD3D
+cd ${gcad_dir_dev}gCAD3D
 tar -xzf ${fnam}
 if [ $? -ne 0 ]; then
   echo "****** ERROR unpacking package ${fnam} *******"
   exit 3
+else
+  echo "- example-files unpacked into ${gcad_dir_dev}gCAD3D *******"
 fi
 
  exit 0
